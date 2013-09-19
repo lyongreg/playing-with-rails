ae3eea08dd46b231d025402da5bcf86eaa5fb341
Greg Lyon <lyon.greg@gmail.com>
2013-09-19 10:04:14 -0600

    added unit tests and validations for statuses

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -23,7 +23,14 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should be logged in to create a status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should create status" do
+    sign_in users(:greg)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
@@ -36,12 +43,21 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:greg)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:greg)
     put :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
   end
