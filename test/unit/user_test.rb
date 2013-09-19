a646cd7741fc1a1d0e772ad8828bdd34e22d3652
Greg Lyon <lyon.greg@gmail.com>
2013-09-19 10:21:38 -0600

    fixed bug with user creation on profile name characters

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -27,11 +27,20 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-	user = User.new
+	user = User.new(first_name: 'Greg', last_name: 'Lyon', email: 'lyon.greg2@gmail.com')
+  user.password = user.password_confirmation = 'asdfasdf'
 	user.profile_name = "my profile with spaces"
 
 	assert !user.save
 	assert !user.errors[:profile_name].empty?
 	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+  user = User.new(first_name: 'Greg', last_name: 'Lyon', email: 'lyon.greg2@gmail.com')
+  user.password = user.password_confirmation = 'asdfasdf'
+  user.profile_name = 'greglyon_1'
+  assert user.valid?
+
   end
 end
